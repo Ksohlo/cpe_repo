7068cb1ae4bbec038e3e0345274a4c319907d67b
ksohlo <alabssouls@gmail.com>
2024-08-12 11:37:31 +0100

    cnfiguring

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express';
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
+const url = process.env.MONGO_URL
 
 app.use(cors());
 app.use(express.json());
